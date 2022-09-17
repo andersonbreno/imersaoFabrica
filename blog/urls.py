26431317368a4378bb895d5d7dcdeb1f03289a1c
anderson <andersonbrenotec@gmail.com>
2022-09-17 14:20:21 -0300

    mudanças adicionadas

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import home, create, leitura_postagem, update, delete
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', home, name='home.html'),
     path('create/', create, name='create'),
     path('post/<int:pk>/', leitura_postagem, name='leitura_postagem'),
     path('update/<int:pk>/', update, name='update'),
